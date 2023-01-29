6818a500d6c0dc39c2dcf4dc775d425f276d307a
To2003 <soytomasaguilar@gmail.com>
2023-01-29 20:54:31 -0300

    Fetch arreglos 1

--- jsonTeams.js
+++ jsonTeams.js
@@ -1,5 +1,4 @@
 const listadoA = document.querySelector("#listadoA");
-const teams = fetch("./teams.json");
 
 const constJson = async () => {
     const resp = await fetch("./teams.json");
@@ -7,9 +6,6 @@ const constJson = async () => {
     
     var grupoA = data.filter(function(el) {
         return (el.grupo === "A");
-    });
-    var grupoB = data.filter(function(el) {
-        return (el.grupo === "B");
     });
 
     grupoA.forEach((post)=> {
