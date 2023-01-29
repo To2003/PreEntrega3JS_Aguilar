39dacb836044850cc7767ec3e0857b504faf385e
To2003 <soytomasaguilar@gmail.com>
2023-01-29 20:53:06 -0300

    Fetch funcionando

--- jsonTeams.js
+++ jsonTeams.js
@@ -1,23 +1,28 @@
-/*const listado = document.querySelector("#listado");
+const listadoA = document.querySelector("#listadoA");
+const teams = fetch("./teams.json");
 
 const constJson = async () => {
     const resp = await fetch("./teams.json");
     const data = await resp.json();
+    
+    var grupoA = data.filter(function(el) {
+        return (el.grupo === "A");
+    });
+    var grupoB = data.filter(function(el) {
+        return (el.grupo === "B");
+    });
 
-    data.forEach((post)=> {
+    grupoA.forEach((post)=> {
         const li = document.createElement("li");
         li.innerHTML = `
-                <h2 class="paises">${post.pais}</h2>
-
+            <section class="sectionPaises">
+                <img src=${post.bandera} alt="Bandera del Pais">
+                <p class="paises">${post.pais}</p>
+            </section>
         `;
 
-
-
-
-
-        listado.append(li);
+        listadoA.append(li);
     });
 };
 
 constJson();
-*/
